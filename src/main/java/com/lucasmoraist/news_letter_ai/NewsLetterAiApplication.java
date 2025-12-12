1b88cae3daaa4ee6348298d5e2ba0a61b88b0e0d
lucasmoraist <luksmnt1101@gmail.com>
2025-12-12 19:09:06 -0300

    Initial project

---
+++ src/main/java/com/lucasmoraist/news_letter_ai/NewsLetterAiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.news_letter_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsLetterAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsLetterAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoraist/news_letter_ai/NewsLetterAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.news_letter_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsLetterAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
