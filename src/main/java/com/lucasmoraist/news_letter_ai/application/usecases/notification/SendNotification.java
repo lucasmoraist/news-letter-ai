d17d4caf30880c8f221f687748e385b38efd37d6
lucasmoraist <luksmnt1101@gmail.com>
2025-12-13 14:50:01 -0300

    feat: implement SendNotification use case for sending notifications to customers

---
+++ src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/notification/SendNotification.java
@@ -0,0 +1,22 @@
+package com.lucasmoraist.news_letter_ai.application.usecases.notification;
+
+import com.lucasmoraist.news_letter_ai.application.gateway.CustomerPersistence;
+import com.lucasmoraist.news_letter_ai.application.gateway.NotificationService;
+import com.lucasmoraist.news_letter_ai.domain.model.Customer;
+import com.lucasmoraist.news_letter_ai.domain.model.Notice;
+
+import java.util.List;
+
+public record SendNotification(CustomerPersistence customerPersistence, NotificationService notificationService) {
+
+    public void execute(List<Notice> notices) {
+        List<Customer> customers = customerPersistence.findAllCustomers();
+
+        customers.forEach(customer ->
+                notices.forEach(notice ->
+                        notificationService.sendNotification(customer.email(), notice)
+                )
+        );
+    }
+
+}
