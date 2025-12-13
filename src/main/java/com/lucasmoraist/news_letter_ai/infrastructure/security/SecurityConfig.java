4349b592db53a63b14c8671f5ca4ea4aecbc745a
lucasmoraist <luksmnt1101@gmail.com>
2025-12-13 18:04:10 -0300

    feat: add Spring Security dependencies for enhanced application security

---
+++ src/main/java/com/lucasmoraist/news_letter_ai/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.lucasmoraist.news_letter_ai.infrastructure.security;
+
+import com.lucasmoraist.news_letter_ai.infrastructure.security.properties.AppProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AppProperties appProperties;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> Optional
+                        .ofNullable(appProperties.getSecurityIgnore())
+                        .map(Map::values)
+                        .filter(x -> !x.isEmpty())
+                        .map(x -> x.toArray(new String[0]))
+                        .ifPresentOrElse(
+                                it -> auth.requestMatchers(it).permitAll().anyRequest().authenticated(),
+                                () -> auth.anyRequest().authenticated()
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173/", "https://talentpool-production-91e8.up.railway.app/", "https://sobeicurriculo.org/"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
