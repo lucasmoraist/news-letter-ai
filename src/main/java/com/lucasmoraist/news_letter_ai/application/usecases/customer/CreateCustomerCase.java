e2cdbeb158419811f67a6db66c2687a391e9b53b
lucasmoraist <luksmnt1101@gmail.com>
2025-12-13 14:48:17 -0300

    chore: add TODOs for welcome notification and latest news retrieval in CreateCustomerCase

--- src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/customer/CreateCustomerCase.java
+++ src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/customer/CreateCustomerCase.java
@@ -7,6 +7,8 @@ public record CreateCustomerCase(CustomerPersistence customerPersistence) {
 
     public void execute(Customer customer) {
         this.customerPersistence.saveCustomer(customer);
+        // TODO: Deve enviar notificação de boas vindas
+        // TODO: Deve pegar as ultimas noticias e enviar para o cliente novo
     }
 
 }
