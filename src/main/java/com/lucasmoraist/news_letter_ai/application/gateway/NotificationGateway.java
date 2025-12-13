9c1fa061ed928809a37cefa56f3c564d639e5890
lucasmoraist <luksmnt1101@gmail.com>
2025-12-13 15:07:51 -0300

    refactor: rename NotificationService to NotificationGateway for clarity

--- src/main/java/com/lucasmoraist/news_letter_ai/application/gateway/NotificationGateway.java
+++ src/main/java/com/lucasmoraist/news_letter_ai/application/gateway/NotificationGateway.java
@@ -2,6 +2,6 @@ package com.lucasmoraist.news_letter_ai.application.gateway;
 
 import com.lucasmoraist.news_letter_ai.domain.model.Notice;
 
-public interface NotificationService {
+public interface NotificationGateway {
     void sendNotification(String email, Notice notice);
 }
--- src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/notification/SendNotification.java
+++ src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/notification/SendNotification.java
@@ -1,13 +1,13 @@
 package com.lucasmoraist.news_letter_ai.application.usecases.notification;
 
 import com.lucasmoraist.news_letter_ai.application.gateway.CustomerPersistence;
-import com.lucasmoraist.news_letter_ai.application.gateway.NotificationService;
+import com.lucasmoraist.news_letter_ai.application.gateway.NotificationGateway;
 import com.lucasmoraist.news_letter_ai.domain.model.Customer;
 import com.lucasmoraist.news_letter_ai.domain.model.Notice;
 
 import java.util.List;
 
-public record SendNotification(CustomerPersistence customerPersistence, NotificationService notificationService) {
+public record SendNotification(CustomerPersistence customerPersistence, NotificationGateway notificationService) {
 
     public void execute(List<Notice> notices) {
         List<Customer> customers = customerPersistence.findAllCustomers();
