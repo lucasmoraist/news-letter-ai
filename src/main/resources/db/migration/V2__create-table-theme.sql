86a0f0a85f5eae5d4a89ec29cb529421f8112ced
lucasmoraist <luksmnt1101@gmail.com>
2025-12-17 22:46:27 -0300

    feat: create t_theme table with constraints and index

---
+++ src/main/resources/db/migration/V2__create-table-theme.sql
@@ -0,0 +1,13 @@
+CREATE TABLE t_theme (
+    id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    customer_id UUID NOT NULL,
+
+    CONSTRAINT pk_t_theme PRIMARY KEY (id),
+    CONSTRAINT uk_t_theme_name UNIQUE (name),
+
+    CONSTRAINT fk_theme_on_customer FOREIGN KEY (customer_id)
+        REFERENCES t_customer (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_theme_customer_id ON t_theme(customer_id);
