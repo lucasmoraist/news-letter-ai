cebbfd1c606afc70d1a6d6e2a2c4a8479b00454e
lucasmoraist <luksmnt1101@gmail.com>
2025-12-22 19:50:21 -0300

    feat: update saveCustomer endpoint to use RequestBody for CustomerDTO

--- src/main/java/com/lucasmoraist/news_letter_ai/infrastructure/web/routes/CustomerRoutes.java
+++ src/main/java/com/lucasmoraist/news_letter_ai/infrastructure/web/routes/CustomerRoutes.java
@@ -3,13 +3,13 @@ package com.lucasmoraist.news_letter_ai.infrastructure.web.routes;
 import com.lucasmoraist.news_letter_ai.infrastructure.web.dto.CustomerDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/customer")
 public interface CustomerRoutes {
 
     @PostMapping("/save")
-    ResponseEntity<Void> saveCustomer(@ModelAttribute("customer") @Valid CustomerDTO dto);
+    ResponseEntity<Void> saveCustomer(@RequestBody @Valid CustomerDTO dto);
 }
