f626c92b9353a79be48975b8960a4c25f88a485b
lucasmoraist <luksmnt1101@gmail.com>
2025-12-22 19:44:17 -0300

    feat: update notice generation to include theme description in prompt

--- src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/notice/GenerateNoticeCase.java
+++ src/main/java/com/lucasmoraist/news_letter_ai/application/usecases/notice/GenerateNoticeCase.java
@@ -2,6 +2,7 @@ package com.lucasmoraist.news_letter_ai.application.usecases.notice;
 
 import com.lucasmoraist.news_letter_ai.application.gateway.GenAIGateway;
 import com.lucasmoraist.news_letter_ai.application.utils.PromptUtils;
+import com.lucasmoraist.news_letter_ai.domain.enums.ThemeEnum;
 import org.springframework.cache.annotation.Cacheable;
 
 import java.time.LocalDate;
@@ -19,9 +20,11 @@ public class GenerateNoticeCase {
             key = "'daily-news'"
     )
     public String execute() {
-        final String loadPrompt = PromptUtils.loadPromptTemplate("prompts/finance-prompt.txt");
+        final String loadPrompt = PromptUtils.loadPromptTemplate("prompts/notice-prompt.txt");
         final LocalDate yesterday = LocalDate.now().minusDays(1);
-        final String prompt = loadPrompt.replace("{{DATE}}", yesterday.toString());
+        final String prompt = loadPrompt
+                .replace("{{DATE}}", yesterday.toString())
+                .replace("{{THEME}}", ThemeEnum.FINANCE.getDescription());
 
         return genAIGateway.sendPromptToGemini(prompt);
     }
